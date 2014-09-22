4f713a954267e44e932b969292e47b128fd4ef58
root <root@Randco.(none)>
2014-09-22 15:40:35 +0200

    MAJ

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Fichier "vote-nir"
+# Fichier "install.sh"
 
 apt-get update
 apt-get upgrade
@@ -8,8 +8,7 @@ mkdir /home/distrib
 wget -P /home/distrib/ http://nginx.org/download/nginx-1.7.4.tar.gz &&
 cd /home/distrib/ && tar -xvpzf nginx-1.7.4.tar.gz
 cd /home/distrib/nginx-1.7.4 && ./configure &&
-cd /home/distrib/nginx-1.7.4 && make &&
-cd /home/distrib/nginx-1.7.4 && make install &&
+make && make install &&
 ln -s /usr/local/nginx/sbin/nginx /usr/local/sbin/nginx
 cp /home/install/nginx /etc/init.d/ &&
 chmod 755 /etc/init.d/nginx
