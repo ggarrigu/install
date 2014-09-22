c73e7ba2841f8743fd54e4a987ce80eefc969e05
root <root@Randco.(none)>
2014-09-22 15:28:57 +0200

    modif bash

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 # Fichier "vote-nir"
 
-echo | apt-get update
-echo | apt-get upgrade
-echo | apt-get install build-essential libcurl3 apache2-threaded-dev libtool dh-autoreconf libxml2 libxml2-dev libxml2-utils libaprutil1 libaprutil1-dev
-echo | mkdir /home/distrib &&
-echo | wget -P /home/distrib/ http://nginx.org/download/nginx-1.7.4.tar.gz
-echo | tar -xvpzf /home/distrib/nginx-1.4.2.tar.gz &&
-echo | /home/distrib/nginx-1.4.2.tar.gz/configure &&
-echo | /home/distrib/nginx-1.4.2.tar.gz/make &&
-echo | /home/distrib/nginx-1.4.2.tar.gz/make install &&
-echo | ln -s /usr/local/nginx/sbin/nginx /usr/local/sbin/nginx
-echo | git clone https://github.com/ggarrigu/VM-install.git /home/distrib/ &&
-echo | cp /home/distrib/VM-install/nginx /etc/init.d/ &&
-echo | chmod 755 /etc/init.d/nginx
-echo | chown root:root /etc/init.d/nginx
-echo | /usr/local/sbin/nginx &&
-echo | service nginx restart
+apt-get update
+apt-get upgrade
+apt-get install build-essential libcurl3 apache2-threaded-dev libtool dh-autoreconf libxml2 libxml2-dev libxml2-utils libaprutil1 libaprutil1-dev
+mkdir /home/distrib
+wget -P /home/distrib/ http://nginx.org/download/nginx-1.7.4.tar.gz &&
+cd /home/distrib/ && tar -xvpzf nginx-1.7.4.tar.gz
+cd /home/distrib/nginx-1.7.4 && ./configure &&
+cd /home/distrib/nginx-1.7.4 && make &&
+cd /home/distrib/nginx-1.7.4 && make install &&
+ln -s /usr/local/nginx/sbin/nginx /usr/local/sbin/nginx
+cp /home/install/nginx /etc/init.d/ &&
+chmod 755 /etc/init.d/nginx
+/usr/local/sbin/nginx &&
+service nginx restart
