8cd8489212c3981989d3ff48391be9e9bcbf5670
root <root@Randco.(none)>
2014-10-10 17:16:49 +0200

    final nginx + mod_sec w/OWASP top 10

--- install.sh
+++ install.sh
@@ -5,38 +5,37 @@ apt-get update
 apt-get upgrade
 apt-get install -y build-essential libcurl4-gnutls-dev apache2-threaded-dev libtool dh-autoreconf libxml2 libxml2-dev libxml2-utils libaprutil1 libaprutil1-dev
 mkdir /home/distrib
+mkdir /etc/nginx
 
 wget -P /home/distrib/ http://nginx.org/download/nginx-1.7.4.tar.gz &&
 cd /home/distrib/ && tar -xvpzf nginx-1.7.4.tar.gz &&
-cd /home/distrib/nginx-1.7.4 && ./configure &&
+cd /home/distrib/nginx-1.7.4 && ./configure --sbin-path=/usr/local/sbin/nginx --conf-path=/etc/nginx/conf/nginx.conf --pid-path=/run/nginx.pid &&
 make && make install &&
-cp /home/install/nginx /etc/init.d/ &&
+wget -P /etc/init.d/ https://raw.githubusercontent.com/Fleshgrinder/nginx-sysvinit-script/master/nginx
 chmod 755 /etc/init.d/nginx
-cd /home/distrib/
 
+cd /home/distrib/ &&
 git clone https://github.com/SpiderLabs/ModSecurity.git mod_security &&
 cd ./mod_security/ &&
 ./autogen.sh &&
 ./configure --enable-standalone-module --disable-apache2-module &&
 make &&
 
-cd /home/distrib/nginx-1.7.4 &&
-./configure --add-module=../mod_security/nginx/modsecurity &&
+cd /home/distrib/nginx-1.7.4 && ./configure --sbin-path=/usr/local/sbin/nginx --conf-path=/etc/nginx/conf/nginx.conf --pid-path=/run/nginx.pid --add-module=../mod_security/nginx/modsecurity &&
 make && make install &&
-rm -rf /usr/local/nginx/conf/nginx.conf
-cp /home/install/nginx.conf /usr/local/nginx/conf/
-cp /home/distrib/mod_security/modsecurity.conf-recommended /usr/local/nginx/conf/
-mv /usr/local/nginx/conf/modsecurity.conf-recommended /usr/local/nginx/conf/modsecurity.conf
-cp /home/distrib/mod_security/unicode.mapping /usr/local/nginx/conf/
-ln -s /usr/local/nginx/sbin/nginx /usr/local/sbin/nginx
-/usr/local/nginx/sbin/nginx
+rm -rf /etc/nginx/conf/nginx.conf
+cp /home/install/nginx.conf /etc/nginx/conf/
+cp /home/distrib/mod_security/modsecurity.conf-recommended /etc/nginx/conf/
+mv /etc/nginx/conf/modsecurity.conf-recommended /etc/nginx/conf/modsecurity.conf
+cp /home/distrib/mod_security/unicode.mapping /etc/nginx/conf/
+/usr/local/sbin/nginx
 service nginx restart
 
 cd /home/distrib/ &&
 git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git
-echo -e "\n\n########## Mod-Security CRS 10 conf ##########" >> /usr/local/nginx/conf/modsecurity.conf
-cat /home/distrib/owasp-modsecurity-crs/modsecurity_crs_10_setup.conf.example >> /usr/local/nginx/conf/modsecurity.conf
-echo -e "\n\n########## Base Mod-Security OWASP CRS ##########" >> /usr/local/nginx/conf/modsecurity.conf
-cat /home/distrib/owasp-modsecurity-crs/base_rules/*.conf >> /usr/local/nginx/conf/modsecurity.conf
-cp /home/distrib/owasp-modsecurity-crs/base_rules/*.data /usr/local/nginx/conf/
+echo -e "\n\n########## Mod-Security CRS 10 conf ##########" >> /etc/nginx/conf/modsecurity.conf
+cat /home/distrib/owasp-modsecurity-crs/modsecurity_crs_10_setup.conf.example >> /etc/nginx/conf/modsecurity.conf
+echo -e "\n\n########## Base Mod-Security OWASP CRS ##########" >> /etc/nginx/conf/modsecurity.conf
+cat /home/distrib/owasp-modsecurity-crs/base_rules/*.conf >> /etc/nginx/conf/modsecurity.conf
+cp /home/distrib/owasp-modsecurity-crs/base_rules/*.data /etc/nginx/conf/
 service nginx restart
